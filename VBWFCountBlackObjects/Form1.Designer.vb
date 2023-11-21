a20a8cb54a794541e887aca0030d178d85e34662
Jon <jd31068@hotmail.com>
2023-11-21 09:26:58 -0500

    Add project files.

---
+++ VBWFCountBlackObjects/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        pbSelectedImage = New PictureBox()
+        btnSelectImage = New Button()
+        btnAskOpenAI = New Button()
+        ofDialog = New OpenFileDialog()
+        txtResponse = New TextBox()
+        CType(pbSelectedImage, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' pbSelectedImage
+        ' 
+        pbSelectedImage.Location = New Point(148, 16)
+        pbSelectedImage.Name = "pbSelectedImage"
+        pbSelectedImage.Size = New Size(511, 346)
+        pbSelectedImage.TabIndex = 0
+        pbSelectedImage.TabStop = False
+        ' 
+        ' btnSelectImage
+        ' 
+        btnSelectImage.Location = New Point(12, 16)
+        btnSelectImage.Name = "btnSelectImage"
+        btnSelectImage.Size = New Size(112, 34)
+        btnSelectImage.TabIndex = 1
+        btnSelectImage.Text = "Select Image"
+        btnSelectImage.UseVisualStyleBackColor = True
+        ' 
+        ' btnAskOpenAI
+        ' 
+        btnAskOpenAI.Location = New Point(12, 66)
+        btnAskOpenAI.Name = "btnAskOpenAI"
+        btnAskOpenAI.Size = New Size(112, 34)
+        btnAskOpenAI.TabIndex = 2
+        btnAskOpenAI.Text = "Ask Open AI"
+        btnAskOpenAI.UseVisualStyleBackColor = True
+        ' 
+        ' txtResponse
+        ' 
+        txtResponse.Location = New Point(149, 378)
+        txtResponse.Name = "txtResponse"
+        txtResponse.Size = New Size(508, 23)
+        txtResponse.TabIndex = 3
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(667, 428)
+        Controls.Add(txtResponse)
+        Controls.Add(btnAskOpenAI)
+        Controls.Add(btnSelectImage)
+        Controls.Add(pbSelectedImage)
+        Name = "Form1"
+        Text = "Form1"
+        CType(pbSelectedImage, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents pbSelectedImage As PictureBox
+    Friend WithEvents btnSelectImage As Button
+    Friend WithEvents btnAskOpenAI As Button
+    Friend WithEvents ofDialog As OpenFileDialog
+    Friend WithEvents txtResponse As TextBox
+
+End Class
---
+++ VBWFCountBlackObjects/Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports System.IO
+Imports System.Net.Http
+Imports System.Security.Policy
+Imports System.Text
+
+Public Class Form1
+    Dim image_path As String
+
+    Public Function ImageToBase64() As String
+        Using ms As New MemoryStream()
+            ' Convert Image to byte[]
+            Dim image As Image = Bitmap.FromFile(image_path)
+            Dim format As System.Drawing.Imaging.ImageFormat = Imaging.ImageFormat.Bmp
+
+            Select Case image_path.Substring(image_path.Length - 4)
+                Case ".png"
+                    format = Imaging.ImageFormat.Png
+                Case ".jpg"
+                    format = Imaging.ImageFormat.Jpeg
+
+            End Select
+
+            image.Save(ms, format)
+            Dim imageBytes As Byte() = ms.ToArray() ' Convert byte[] to Base64 String
+            Dim base64String As String = Convert.ToBase64String(imageBytes)
+            Return base64String
+        End Using
+    End Function
+
+    Private Async Function SendToOpenAI() As Task(Of String)
+
+        Dim openAIkey = "********* YOUR OPEN AI KEY GOES HERE ********************"
+        Dim base64_image As String = ImageToBase64()
+        Dim url = "https://api.openai.com/v1/chat/completions"
+        Dim responseContent
+
+        Using client As HttpClient = New HttpClient()
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {openAIkey}")
+
+            Dim payload = New With
+            {
+                .model = "gpt-4-vision-preview",
+                .messages =
+                {
+                    New With
+                    {
+                        .role = "user",
+                        .content =
+                        {
+                            New With
+                            {
+                                .type = "text",
+                                .text = "How many black spots are in this image?"
+                            },
+                            New With
+                            {
+                                .type = "image_url",
+                                .image_url = New With
+                                {
+                                    .url = $"data:image/jpeg;base64,{base64_image}"
+                                }
+                            }
+                        }
+                    }
+                },
+                .max_tokens = 300
+            }
+
+            Dim jsonPayload = New StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json")
+            Dim response = Await client.PostAsync(url, jsonPayload)
+            responseContent = Await response.Content.ReadAsStringAsync()
+
+        End Using
+
+        Return responseContent
+
+    End Function
+    Private Sub btnSelectImage_Click(sender As Object, e As EventArgs) Handles btnSelectImage.Click
+
+        ' ask for the image file
+        If ofDialog.ShowDialog() = DialogResult.OK Then
+            image_path = ofDialog.FileName
+            pbSelectedImage.Load(ofDialog.FileName)
+        End If
+
+    End Sub
+
+    Private Async Sub btnAskOpenAI_Click(sender As Object, e As EventArgs) Handles btnAskOpenAI.Click
+
+        ' post the image to Open AI and ask it how many black objects there are
+        txtResponse.Text = "Asking Open AI"
+        Application.DoEvents()
+
+        txtResponse.Text = Await SendToOpenAI()
+
+    End Sub
+End Class
